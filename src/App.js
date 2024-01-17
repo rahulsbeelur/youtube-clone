046ef0e76a050c626b4fe1f2ae87f40f08cf252f
rahulsbeelur <srahulbeelur@gmail.com>
2024-01-17 14:55:29 +0530

    Layout skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
-    return <div className="text-[24px] font-bold">Hello World!</div>;
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    );
 }
 
 export default App;
